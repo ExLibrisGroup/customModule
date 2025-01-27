306bb8b9d8a5a64145214aa705481e7358e46d6f
shoul.manevitz <shoul.manevitz@clarivate.com>
2025-01-27 10:10:32 +0200

    JIRA: URM-1234 Developer:shoul.manevitz Description: add dynamic providers to Angular bootstrap

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,7 +1,6 @@
 import "@angular/compiler";
 import { AppModule } from './app/app.module';
 import {bootstrap} from "@angular-architects/module-federation-tools";
-import {Store, StoreModule} from "@ngrx/store";
 
 export const bootstrapRemoteApp = (providers: any) => {
    return bootstrap(AppModule(providers), {
