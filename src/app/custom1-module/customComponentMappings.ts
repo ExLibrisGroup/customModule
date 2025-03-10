267eab3aaa0ea0b30cdd855cb9b9892501becbf0
david.bendavid <david.bendavid@clarivate.com>
2025-03-10 17:09:05 +0200

    Updated webpack config, environment settings, and dependencies2

--- src/app/custom1-module/customComponentMappings.ts
+++ src/app/custom1-module/customComponentMappings.ts
@@ -1,16 +1,5 @@
-import { FullDisplayDetailsComponentDist } from "../full-display-details/full-display-details.component";
-import { TestAfterComponent } from "../test-after/test-after.component";
-import { TestBeforeComponent } from "../test-before/test-before.component";
-import { TestBottomComponent } from "../test-bottom/test-bottom.component";
-import { TestTopComponent } from "../test-top/test-top.component";
-
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
-  ["nde-full-display-details",FullDisplayDetailsComponentDist],
-  ["nde-full-display-details-after",TestAfterComponent],
-  ["nde-full-display-details-top",TestTopComponent],
-  ["nde-full-display-details-before",TestBeforeComponent],
-  ["nde-full-display-details-bottom",TestBottomComponent]
 
 
 
