5f1a4311ea514d59d55150de9922106251033325
Danielle Books <danielle@thirdiron.com>
2025-03-27 15:22:18 -0600

    commit main adapter tweak

--- src/app/third-iron-adapter/third-iron-adapter.component.ts
+++ src/app/third-iron-adapter/third-iron-adapter.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { createFeatureSelector, createSelector, Store } from '@ngrx/store';
 import { map, Observable, toArray } from 'rxjs';
-import { SingleButtonComponent } from '../single-button/single-button.component';
+import { SingleButtonComponent } from '../components/single-button/single-button.component';
 
 type SearchItemsState = { entities: Record<string, object> };
 // type FullDisplayStater = { selectedRecordId: string };
@@ -10,8 +10,15 @@ export type ButtonInfo = {
   ariaLabel: string;
   buttonText: string;
   url: string;
-  icon?: string;
 };
+
+// TODO rename these assets?
+// enum ImageUrl {
+//   BrowzineBook = 'https://assets.thirdiron.com/images/integrations/browzine-open-book-icon.svg',
+//   PDF = 'https://assets.thirdiron.com/images/integrations/browzine-pdf-download-icon.svg',
+//   ArticleAlert = 'https://assets.thirdiron.com/images/integrations/browzine-retraction-watch-icon.svg',
+//   ArticleDefault = 'https://assets.thirdiron.com/images/integrations/browzine-article-link-icon.svg',
+// }
 
 const selectSearchState = createFeatureSelector<SearchItemsState>('Search');
 const selectSearchEntities = createSelector(
@@ -26,10 +33,17 @@ const selectSearchEntities = createSelector(
   styleUrl: './third-iron-adapter.component.scss',
 })
 export class ThirdIronAdapterComponent {
-  buttonInfo = signal({
-    url: 'https://libkey.io/libraries/322/articles/540512060/full-text-file?utm_source=api_193',
-    buttonText: 'Test Third Iron button',
+  // TODO - load info instead of hardcorded
+  buttonInfo1 = signal({
     ariaLabel: 'A test aria label',
+    buttonText: 'Test Third Iron button',
+    url: 'https://libkey.io/libraries/322/articles/540512060/full-text-file?utm_source=api_193',
+  });
+
+  buttonInfo2 = signal({
+    ariaLabel: 'A second test aria label',
+    buttonText: 'Second test button',
+    url: 'https://libkey.io/libraries/322/articles/540512060/full-text-file?utm_source=api_193',
   });
 
   searchItemsState$: Observable<SearchItemsState> | undefined;
