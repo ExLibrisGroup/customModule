0033ccbece7141dbb30c5078b5e375191a974b3e
Danielle Books <danielle@thirdiron.com>
2025-04-07 16:00:45 -0600

    add provideHttpClient to provider list
    auto format from prettier also done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,33 @@
-import {ApplicationRef, DoBootstrap, Injector, ModuleWithProviders, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
-import {createCustomElement, NgElementConstructor} from "@angular/elements";
-import {Router} from "@angular/router";
-import {selectorComponentMap} from "./custom1-module/customComponentMappings";
-import {TranslateModule} from "@ngx-translate/core";
+import {
+  ApplicationRef,
+  DoBootstrap,
+  Injector,
+  ModuleWithProviders,
+  NgModule,
+} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { createCustomElement, NgElementConstructor } from '@angular/elements';
+import { Router } from '@angular/router';
+import { selectorComponentMap } from './custom1-module/customComponentMappings';
+import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { AutoAssetSrcDirective } from './services/auto-asset-src.directive';
+import { provideHttpClient } from '@angular/common/http';
 
-export const AppModule = ({providers}: {providers:any}) => {
-   @NgModule({
-    declarations: [
-      AppComponent,
-      AutoAssetSrcDirective
-    ],
+export const AppModule = ({ providers }: { providers: any }) => {
+  @NgModule({
+    declarations: [AppComponent, AutoAssetSrcDirective],
     exports: [AutoAssetSrcDirective],
-    imports: [
-      BrowserModule,
-      CommonModule,
-      TranslateModule.forRoot({})
-    ],
-    providers: providers,
-    bootstrap: []
+    imports: [BrowserModule, CommonModule, TranslateModule.forRoot({})],
+    providers: [...providers, provideHttpClient()],
+    bootstrap: [],
   })
-  class AppModule implements DoBootstrap{
-    private webComponentSelectorMap = new Map<string,  NgElementConstructor<unknown>>();
+  class AppModule implements DoBootstrap {
+    private webComponentSelectorMap = new Map<
+      string,
+      NgElementConstructor<unknown>
+    >();
 
     constructor(private injector: Injector, private router: Router) {
       router.dispose(); //this prevents the router from being initialized and interfering with the shell app router
@@ -32,7 +35,9 @@ export const AppModule = ({providers}: {providers:any}) => {
 
     ngDoBootstrap(appRef: ApplicationRef) {
       for (const [key, value] of selectorComponentMap) {
-        const customElement = createCustomElement(value, {injector: this.injector});
+        const customElement = createCustomElement(value, {
+          injector: this.injector,
+        });
         this.webComponentSelectorMap.set(key, customElement);
       }
     }
@@ -41,10 +46,9 @@ export const AppModule = ({providers}: {providers:any}) => {
      * Use componentMapping, selectorComponentMap
      * @param componentName
      */
-    public getComponentRef(componentName:string) {
+    public getComponentRef(componentName: string) {
       return this.webComponentSelectorMap.get(componentName);
     }
   }
-  return AppModule
-}
-
+  return AppModule;
+};
