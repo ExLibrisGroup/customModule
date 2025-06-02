5ca6c163bae1f04ba7579a05a22cbb9f56dd5867
Danielle Books <danielle@thirdiron.com>
2025-06-02 16:14:31 -0600

    fix base-button component tests

--- src/app/components/base-button/base-button.component.spec.ts
+++ src/app/components/base-button/base-button.component.spec.ts
@@ -2,9 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BaseButtonComponent } from './base-button.component';
 import { ComponentRef } from '@angular/core';
-import { ButtonInfo } from 'src/app/types/buttonInfo.types';
 import { ButtonType } from 'src/app/shared/button-type.enum';
 import { EntityType } from 'src/app/shared/entity-type.enum';
+import { IconType } from 'src/app/shared/icon-type.enum';
 
 // @Component({
 
@@ -12,7 +12,7 @@ import { EntityType } from 'src/app/shared/entity-type.enum';
 
 const validateButtonProps = (
   buttonElement: HTMLElement,
-  expectedValues: ButtonInfo
+  expectedValues: any
 ) => {
   const buttonTextSpan = buttonElement.querySelector(
     '[data-testid="ti-button-text"]'
@@ -26,10 +26,10 @@ const validateButtonProps = (
   const iconColor = buttonIcon?.getAttribute('ng-reflect-color');
   const iconName = buttonIcon?.getAttribute('ng-reflect-name');
 
-  expect(buttonAriaLabel).toContain(expectedValues.ariaLabel);
-  expect(buttonTextSpan.textContent).toContain(expectedValues.buttonText);
-  expect(iconColor).toContain(expectedValues.color);
-  expect(iconName).toContain(expectedValues.icon);
+  expect(buttonAriaLabel).toContain(expectedValues.mainAriaLabel);
+  expect(buttonTextSpan.textContent).toContain(expectedValues.mainButtonText);
+  expect(iconColor).toContain(expectedValues.mainColor);
+  expect(iconName).toContain(expectedValues.mainIcon);
 };
 
 describe('BaseButtonComponent', () => {
@@ -45,30 +45,68 @@ describe('BaseButtonComponent', () => {
     fixture = TestBed.createComponent(BaseButtonComponent);
     component = fixture.componentInstance;
     componentRef = fixture.componentRef;
-
-    componentRef.setInput('ariaLabel', 'Arial Label Text!');
-    componentRef.setInput('buttonText', 'Test Button Text!');
-    componentRef.setInput('color', 'sys-primary');
-    componentRef.setInput('icon', 'article-link-icon');
-    componentRef.setInput('url', 'www.test.com');
   });
 
   it('should create', () => {
     expect(component).toBeDefined();
   });
 
-  it('should have expected button properties', () => {
+  it('should have expected button properties for ArticleLink', () => {
+    componentRef.setInput('ariaLabel', 'Arial Label Text!');
+    componentRef.setInput('buttonText', 'Test Button Text!');
+    componentRef.setInput('color', 'sys-primary');
+    componentRef.setInput('icon', IconType.ArticleLink);
+    componentRef.setInput('url', 'www.test.com');
+
     fixture.detectChanges();
     buttonElement = fixture.nativeElement;
 
     validateButtonProps(buttonElement, {
-      ariaLabel: 'Arial Label Text!',
-      buttonText: 'Test Button Text!',
-      color: 'sys-primary',
-      icon: 'article-link-icon',
-      url: 'www.test.com',
-      buttonType: ButtonType.ArticleLink,
+      mainAriaLabel: 'Arial Label Text!',
+      mainButtonText: 'Test Button Text!',
+      mainColor: 'sys-primary',
+      mainIcon: 'article-link-icon',
+      mainUrl: 'www.test.com',
+      mainButtonType: ButtonType.ArticleLink,
       entityType: EntityType.Article,
+    });
+  });
+
+  it('should have expected button properties for DirectToPDF', () => {
+    componentRef.setInput('ariaLabel', 'PDF Label Text!');
+    componentRef.setInput('buttonText', 'PDF Button Text!');
+    componentRef.setInput('color', 'sys-primary');
+    componentRef.setInput('icon', IconType.DownloadPDF);
+    componentRef.setInput('url', 'www.test.com');
+
+    fixture.detectChanges();
+    buttonElement = fixture.nativeElement;
+
+    validateButtonProps(buttonElement, {
+      mainAriaLabel: 'PDF Label Text!',
+      mainButtonText: 'PDF Button Text!',
+      mainColor: 'sys-primary',
+      mainIcon: IconType.DownloadPDF,
+      mainUrl: 'www.test.com',
+    });
+  });
+
+  it('should have expected button properties for Retracted article', () => {
+    componentRef.setInput('ariaLabel', 'Retracted!');
+    componentRef.setInput('buttonText', 'Retracted!');
+    componentRef.setInput('color', 'sys-primary');
+    componentRef.setInput('icon', IconType.ArticleAlert);
+    componentRef.setInput('url', 'www.test.com');
+
+    fixture.detectChanges();
+    buttonElement = fixture.nativeElement;
+
+    validateButtonProps(buttonElement, {
+      mainAriaLabel: 'Retracted!',
+      mainButtonText: 'Retracted!',
+      mainColor: 'sys-primary',
+      mainIcon: IconType.ArticleAlert,
+      mainUrl: 'www.test.com',
     });
   });
 });
