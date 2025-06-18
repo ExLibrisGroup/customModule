9788cbdec71cbaf44e06bad0f12d1d69c36f256b
Jonathan Lake <25333421+Jono120@users.noreply.github.com>
2025-06-18 12:31:03 +1200

    content updates for the pages

--- src/app/custom1-module/customComponentMappings.ts
+++ src/app/custom1-module/customComponentMappings.ts
@@ -1,6 +1,7 @@
+import { TeWaharoaComponent } from '../te-waharoa/te-waharoa.component' //needed
+
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
-
-
-
+    ['nde-recommendations-before', TeWaharoaComponent], // assuming that the component is imported correctly
+    ['nde-recommendations-after', TeWaharoaComponent],
 ]);
