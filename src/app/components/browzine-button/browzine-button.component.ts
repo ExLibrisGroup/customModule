09947c52873762c2b6c22671cdb3d0e5d7bc185a
Danielle Books <danielle@thirdiron.com>
2025-08-19 12:40:46 -0600

    BZ-10083 add stack options to browzine button wrapper

--- src/app/components/browzine-button/browzine-button.component.ts
+++ src/app/components/browzine-button/browzine-button.component.ts
@@ -3,21 +3,42 @@ import { EntityType } from 'src/app/shared/entity-type.enum';
 import { IconType } from 'src/app/shared/icon-type.enum';
 import { BaseButtonComponent } from '../base-button/base-button.component';
 import { TranslationService } from '../../services/translation.service';
+import { CombinedLink } from 'src/app/types/primoViewModel.types';
+import { StackedButtonComponent } from '../stacked-dropdown/components/stacked-button.component';
 
 @Component({
   selector: 'custom-browzine-button',
   standalone: true,
-  imports: [BaseButtonComponent],
+  imports: [BaseButtonComponent, StackedButtonComponent],
   templateUrl: './browzine-button.component.html',
   styleUrl: './browzine-button.component.scss',
 })
 export class BrowzineButtonComponent {
   url = input.required<string>();
   entityType = input.required<EntityType>();
+  stack = input<boolean>(false);
+  stackType = input<'main' | 'dropdown'>('dropdown');
+  link = input<CombinedLink>({
+    entityType: EntityType.Unknown,
+    url: '',
+    label: '',
+  });
 
-  buttonText = computed<string>(() => this.getButtonText(this.entityType()));
   EntityType = EntityType;
   IconType = IconType;
+
+  buttonText = computed<string>(() => this.getButtonText(this.entityType()));
+
+  updatedLink = computed(() => {
+    const originalLink = this.link();
+    const label = this.buttonText() || originalLink.label;
+    return {
+      ...originalLink,
+      label,
+      icon: IconType.BrowZine,
+      source: originalLink.source ?? 'thirdIron',
+    };
+  });
 
   constructor(private translationService: TranslationService) {}
 
