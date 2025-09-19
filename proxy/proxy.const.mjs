f2d2a806278e4bb086f380e9b5019e5c7e3cde48
Adam Shire <adamshire123@gmail.com>
2025-09-19 09:32:36 -0400

    Initial setup for MIT
    Why these changes are being introduced:
    * make sure built customization package is targeting MIT Primo NDE view
    
    How this addresses that need:
    * adds view and inst ID to build settings
    * adds production URL for local testing

--- proxy/proxy.const.mjs
+++ proxy/proxy.const.mjs
@@ -1,7 +1,7 @@
 //set the url of the server you want to test your code with and start the development server using the following command:
 // ng serve --proxy-config ./proxy/proxy.conf.mjs   
 const environments = {
-    'example': 'https://myPrimoVE.com',
-  }
-  
-  export const PROXY_TARGET = environments['example']; 
+  'Production': 'https://mit.primo.exlibrisgroup.com',
+}
+
+export const PROXY_TARGET = environments['Production']; 
