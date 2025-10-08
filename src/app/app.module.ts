49edbc811e3a0b42e34c66f6ceba59d57c29a587
shoul.manevitz <shoul.manevitz@clarivate.com>
2025-10-08 16:39:39 +0300

    Add injection token for shell app router. This is the custom module side of URM-315618 which requires changes in the main app as well

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {ApplicationRef, DoBootstrap, Injector, ModuleWithProviders, NgModule} from '@angular/core';
+import {ApplicationRef, DoBootstrap, Injector, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {createCustomElement, NgElementConstructor} from "@angular/elements";
@@ -7,8 +7,11 @@ import {selectorComponentMap} from "./custom1-module/customComponentMappings";
 import {TranslateModule} from "@ngx-translate/core";
 import { CommonModule } from '@angular/common';
 import { AutoAssetSrcDirective } from './services/auto-asset-src.directive';
+import {SHELL_ROUTER} from "./injection-tokens";
 
-export const AppModule = ({providers}: {providers:any}) => {
+
+
+export const AppModule = ({providers, shellRouter}: {providers:any, shellRouter: Router}) => {
    @NgModule({
     declarations: [
       AppComponent,
@@ -20,7 +23,7 @@ export const AppModule = ({providers}: {providers:any}) => {
       CommonModule,
       TranslateModule.forRoot({})
     ],
-    providers: providers,
+    providers: [...providers, {provide: SHELL_ROUTER, useValue: shellRouter}],
     bootstrap: []
   })
   class AppModule implements DoBootstrap{
---
+++ src/app/injection-tokens.ts
@@ -0,0 +1,4 @@
+import {InjectionToken} from "@angular/core";
+import {Router} from "@angular/router";
+
+export const SHELL_ROUTER = new InjectionToken<Router>('SHELL_ROUTER');
